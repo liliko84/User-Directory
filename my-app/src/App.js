848f39233e5688a125aa414a35cb1b0facd2cc13
lili gugushvili <lilikog@yahoo.com>
2020-04-15 17:51:22 -0400

    all files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -25,9 +25,43 @@ class App extends Component {
   filter = () => {
     this.setState({
       filtered: this.state.employees.filter(employee => {
-        return employee.name.first.includes(this.state.search)
+        // Convert first name to uppercase
+        const first = employee.name.first.toUpperCase();
+        // Convert last name to uppercase
+        const last = employee.name.last.toUpperCase();
+        // Convert what user is searching for to uppercase
+        const search = this.state.search.toUpperCase()
+
+        // Now compare the uppercase search to the uppercase values
+        // if the search value is anywhere inside of the first name string return true
+        if (first.includes(search) === true) {
+          return true
+        }
+        // if the search value is anywhere inside of the last name string return true
+        else if (last.includes(search) === true) {
+          return true
+        }
+        // if the search value is NOT found in any value, then return false
+        else {
+          return false
+        }
+
+        // reaturn first.includes(search) || last.includes(search)
       })
     })
+  }
+
+  sort = key => {
+    if (key === 'first') {
+      this.setState({
+        filtered: this.state.filtered.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+      })
+    }
+    else if (key === 'last') {
+      this.setState({
+        filtered: this.state.filtered.sort((a, b) => (a.name.last > b.name.last) ? 1 : -1)
+      })
+    }
   }
 
   handleInputChange = event => {
@@ -43,15 +77,23 @@ class App extends Component {
       <>
         <input type="text" name="search" onChange={this.handleInputChange} />
         <table className="table">
+          <thead>
+            <tr>
+              <th>Avatar</th>
+              <th onClick={() => this.sort('first')}>First Name</th>
+              <th onClick={() => this.sort('last')}>Last Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
           <tbody>
             {
-              this.state.filtered.map((employee, i, arr) => <tr key={i}>
-                <td>{arr[i].name.first}</td>
+              this.state.filtered.map((employee, i) => <tr key={i}>
+                <td><img src={employee.picture.thumbnail} alt="" /></td>
+                <td>{employee.name.first}</td>
                 <td>{employee.name.last}</td>
                 <td>{employee.email}</td>
-                <td>{employee.location.street.number} {employee.location.street.name}</td>
                 <td>{employee.phone}</td>
-                <td><img src={employee.picture.thumbnail} alt="" /></td>
+                <td>{employee.location.street.number} {employee.location.street.name}</td>
               </tr>)
             }
           </tbody>
